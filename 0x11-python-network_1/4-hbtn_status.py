4c94d1b2bd2ce556eef1a9c5315076796939143d
Prulm <jaredyared83@gmail.com>
2022-09-23 22:44:00 +0300

    added project source files

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@
 import requests
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
+    url = "https://alx-intranet.hbtn.io/status"
     r = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
